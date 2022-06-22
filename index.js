8d1be3c336737734b2a390da213acccb0e59de65
Serg <>
2022-06-22 13:02:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let username = "my name";
+let bonusBalance = 1000;
+
+console.log(`Пользователь: ${username}`);
+console.log(`Баланс: ${bonusBalance}`);
+
+const bonusBuysNew = 50;
+const bonusDropsEveryDay = 3;
+
+const days = 7;               //Количество дней для расчета баланса
+const daysBetweenBuys = 2;    //Каждые n дней покупка
+  
+for (let i = 0; i < days; i++)  {
+  if (i % daysBetweenBuys) 
+    bonusBalance += bonusBuysNew;      //Бонусы за покупку
+  
+  bonusBalance -= bonusDropsEveryDay;  //Списание
+}
+
+console.log(`Баланс на ${days} день: ${bonusBalance}`);
+
